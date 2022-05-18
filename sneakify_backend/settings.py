8a85c02ba61acdfd86b430307c83af24f165bf6d
chidera <greyspades99@gmail.com>
2022-05-18 16:40:09 +0100

    changed url for admin page

--- sneakify_backend/settings.py
+++ sneakify_backend/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '((vcl48r_!q-u=b^+=47--rcx2rn$2na-+u8cktc_i=my4j)p3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','localhost']
 
 
 # Application definition
